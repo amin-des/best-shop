f7079a6dc4ad10bc2ca843683d4ef82545cd7d7a
Amin Salimi <aminsalimiwork@gmail.com>
2025-09-19 16:16:58 +0330

    ok

--- backend/blogs/models.py
+++ backend/blogs/models.py
@@ -4,9 +4,13 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UsersPosts(models.Model):
-    is_published = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
     is_delete = models.BooleanField(default=False)
     author = models.ForeignKey(User , on_delete=models.CASCADE)
     publish_date = models.DateField(auto_now=True)
     title = models.CharField(max_length=200)
     main_text = models.TextField()
+    
+    def __str__(self):
+        return self.author.username
+    
